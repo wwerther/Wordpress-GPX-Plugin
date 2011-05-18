f2ec61f63ee83a0a798c94bea98290ef14bdc3b4
Walter Werther <walter.werther@gmx.de>
2011-05-18 11:34:53 +0200

    Prepare to tag 0.1.3

--- tag.sh
+++ tag.sh
@@ -2,6 +2,7 @@
 # vim: set ts=4 et nu ai ff=unix backupcopy=yes syntax=sh indentexpr= :vim
 
 TAG=$1
+TAGFORCE=$2
 
 if [ -z "$TAG" ]; then
 	echo "Kein Tag mit angegeben"
@@ -13,7 +14,9 @@ sed -i -e "s/^\\(Version:\\).\+/\1 $TAG/" ww_gpx_infos.php
 
 git commit -a -m "Prepare to tag $TAG"
 git push
-git tag -a $TAG -m "Tag to $TAG"
+
+
+git tag $TAGFORCE -a $TAG -m "Tag to $TAG"
 git push --tags
 
 # ./updatewordpress.sh $TAG
